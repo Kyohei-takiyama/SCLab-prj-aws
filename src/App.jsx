0d464a11478ab47cb18aa65de30b370867e58261
kyohei-takiyama <takiyamakyohei@gmail.com>
2022-05-21 17:56:23 +0900

    ReactRouterを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,49 @@
-// Material UI
-import {
-  Typography,
-  AppBar,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  CssBaseline,
-  Grid,
-  Toolbar,
-  Container,
-  Button,
-} from "@material-ui/core";
-import { Favorite } from "@material-ui/icons";
-
+/**
+ * @description
+ *  フォルダ名はキャメルケース、ファイル名はアッパーキャメル
+ *  コンポーネントファイルを作成する場合は、拡張子はjsxがおすすめです
+ *  コンポーネントのひな型コマンド　rfce
+ */
 import "./App.css";
 
-// スタイル設定
-import { useStyles } from "./styles";
+// 外部ライブラリ
+// マテリアルUI
+import Material from "./materialuisample/Material";
 
-// カード初期値
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+/**
+ * @author 瀧山
+ * @description
+ * 　Router : Router配下でルーティングが出来るようになる
+ *   Routes : ルーティング定義の親
+ *   Route : ルーティング定義の子
+ *    @param path="パスを記載" element={<ルーティング先のコンポーネントを記載>}
+ *   Link : aタグのリアクトルータ版
+ *    @param to="パスを記載"
+ *  useNavigate : ナビゲートさせる関数 使用方法はMaterialページ参照
+ *  useParams : パスパラメータを取得 使用方法はProfileページ参照
+ */
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
+// コンポーネント
+import Home from "./pages/home/Home";
+import Notfoundpage from "./pages/error/404/Notfoundpage";
+import Profile from "./pages/profile/Profile";
 
 function App() {
-  const classes = useStyles();
   return (
-    <>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Favorite className={classes.icon} />
-          <Typography variant="h6" style={{ margin: "0 10px" }}>
-            Service Cloud Lab
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        <div className={classes.container}>
-          {/* <Container maxWidth="sm" style={{ marginTop: "100px" }}> */}
-          <Container maxWidth="sm">
-            <Typography
-              variant="h2"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-            >
-              Service Cloud Lab
-            </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="textSecoundary"
-              paragraph
-            >
-              Service Cloud Lab
-            </Typography>
-            <div className={classes.buttons}>
-              <Grid container spacing={2} justifyContent="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    投稿を見る
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    投稿を見る
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </div>
-        <Container className={classes.cardGrid} maxWidth="md">
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5">
-                      Header
-                    </Typography>
-                    <Typography gutterBottom variant="h5">
-                      bodyddddddddddddddddddddddddddddddddddddddddddddddddd
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          color="text"
-          variant="subtitle1"
-          align="center"
-          gutterBottom
-        >
-          Footer
-        </Typography>
-      </footer>
-    </>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/material">MaterialPage</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/material" element={<Material />} />
+        <Route path="/profile/:userid" element={<Profile />} />
+        <Route path="*" element={<Notfoundpage />} />
+      </Routes>
+      <div>footer</div>
+    </Router>
   );
 }
 
